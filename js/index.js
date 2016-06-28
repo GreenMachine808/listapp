583cc182428f2b42936c729d2daac4736e7286d4
GreenMachine808 <mherrera23@punahou.edu>
2016-06-27 14:26:22 -1000

    Made a list
    Made a button that cycled threw colors displayed in the console.

---
+++ js/index.js
@@ -0,0 +1,30 @@
+var colors = [
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"blue",
+	"indigo",
+	"violet"
+];
+
+
+var count = 0;
+
+
+var cycle = function(array) {
+	console.log(array[count]);
+	if (count == array.length-1) {
+		count = 0;
+	} else {
+		count++;
+	};
+
+
+};
+
+
+
+var show2 = function(){
+	cycle(colors);
+};
